ed184a4c8dc79155b5c04628d8ae9482a30bf4ff
Pivotingmum <joladeosuntubo@gmail.com>
2022-07-11 10:20:32 -0600

    setting up

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request, make_response
+import json
+from db_helpers import run_query
+from flask_cors import CORS
+import os
+import bcrypt
+import uuid
+
+
+
+# configuration
+
+app = Flask(__name__)
+app.testing = True
+
+# enable CORS
+CORS(app, resources={ r'/*': { 'origins': '*' } })
+
+""" from endpoints import client, login, menu_item, order, restaurant  """
+
